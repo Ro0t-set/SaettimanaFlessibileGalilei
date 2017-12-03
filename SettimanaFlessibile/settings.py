8a62b182b7dea6e864d308ebeabc2f675bfe4bca
Tommaso Patriti <ro0trexustom@gmail.com>
2017-12-03 16:49:30 +0100

    Update settings.py

--- SettimanaFlessibile/settings.py
+++ SettimanaFlessibile/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 USE_TZ = True
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'iltavoloblu'
+EMAIL_HOST_PASSWORD = 'pw'
 EMAIL_HOST_USER = 'settimanaflessibile@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
